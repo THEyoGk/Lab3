49e8277da5e176f3d2f3098d7e8987163b594aa2
LehaSever <lehasever@lehasever-HP-ProBook-4530s.(none)>
2015-05-06 13:40:39 +0300

    server part

---
+++ server.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define PORT_NUMBER 5000
+#define BUFFER_SIZE 256
+#define FILENAME_MAX_SIZE 256
+#define MAX_THREAD_COUNT 5
+//#define THREAD
+
+void* sendFile(void* param){
+	char filename[FILENAME_MAX_SIZE];
+	unsigned char buffer[BUFFER_SIZE] = { 0 };
+	FILE *file;
+	int bytes_read = 0, bytes_send = 0;
+	int client_id = (int)param;
+	
+
+	bytes_read = read(client_id, filename, sizeof(filename)-1);
+	if (bytes_read < 0) {
+		printf("Error reading filename.\n");
+		close(client_id);
+		return 0;
+	}
+
+	//Open the file to transfer 
+	filename[bytes_read] = 0;
+	if (access(filename, F_OK) == -1) {
+		printf("File not found.\n");
+		close(client_id);
+		return 0;
+	}
+
+	file = fopen(filename, "rb");
+	if (file == NULL) {
+		printf("File open error.\n");
+		close(client_id);
+		return 0;
+	}
+
+	//Read file and send it
+	for (;;) {
+		bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
+
+		// If read was success, send data.
+		if (bytes_read > 0) {
+			bytes_send = write(client_id, buffer, bytes_read);
+
+			if (bytes_send < bytes_read) {
+				printf("Error sending file.\n");
+				fclose(file);
+				close(client_id);
+				return 0;
+			}
+			
+			printf("Bytes send: %d\n", bytes_send);
+		}
+
+		if (bytes_read < BUFFER_SIZE) {
+			if (feof(file))
+				printf("File send.\n");
+			if (ferror(file))
+				printf("Error reading from file.\n");
+			break;
+		}
+	}
+
+	fclose(file);
+	close(client_id);
+	return 0;
+}
+
+
+int main(int argc, char* argv[]){
+	
+	int socket_id = 0, client_id = 0, yes = 1;
+	struct sockaddr_in serverSocket;
+
+#ifdef THREAD
+	pthread_t threads[MAX_THREAD_COUNT] = { NULL };
+	int error = 0;
+	int i = 0;
+#else
+	pid_t proc_id = 0;
+#endif
+
+	//Creating socket
+	if ((socket_id = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Error : Socket could not be created.\n");
+		return 1;
+	}
+	
+	printf("Socket created.\n");
+	
+	//Initializing socket data 
+	memset(&serverSocket, '0', sizeof(serverSocket));
+	serverSocket.sin_family = AF_INET;
+	serverSocket.sin_addr.s_addr = INADDR_ANY;
+	serverSocket.sin_port = htons(PORT_NUMBER);
+	
+	
+	if (setsockopt(socket_id, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    		printf("Error : Failed while setting socked options.\n");
+		return 1;
+	}
+
+	if ((bind(socket_id, (struct sockaddr*)&serverSocket, sizeof(serverSocket))) < 0) {
+		printf("Error : Binding socket failed.\n");
+		return 1;
+	}
+
+	if (listen(socket_id, 10) == -1) {
+		printf("Error : Listening socket failed.\n");
+		return -1;
+	}
+
+	for (;;) {
+		client_id = accept(socket_id, (struct sockaddr*)NULL, NULL);
+
+		if (client_id < 0) {
+			printf("Error : Accept client failed.\n");
+			continue;
+		}
+		
+#ifdef THREAD
+		
+		for (i = 0; i < MAX_THREAD_COUNT; i++)
+			if ((threads[i] == NULL) || (pthread_kill(threads[i], 0) != ESRCH))
+				break;
+
+		if (i >= MAX_THREAD_COUNT) {
+			printf("Error : There's no free threads.\n");
+			continue;
+		}
+
+		error = pthread_create(&threads[i], NULL, sendFile, (void*)client_id);
+		if (error) {
+			printf("Error : Thread create failed.\n");
+			continue;
+		}
+
+#else
+		switch (proc_id = fork()) {
+		case -1:
+			printf("Error : Process create failed.\n");
+			break;
+		case 0:
+			sendFile((void*)client_id);
+			return 0;
+		default:
+		  	close(client_id);
+			break;
+		}
+		
+#endif
+	}
+	
+	
+	return 0;
+}
